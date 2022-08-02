e6139df7e2eb7ffd7b3738819a04bc330c33a9f4
takaya-47 <local.s.s.47@gmail.com>
2022-08-03 08:33:53 +0900

    ボタンクリック時に押されるべきでないボタンを非活性にすることでバグを修正

--- js/stopWatch.js
+++ js/stopWatch.js
@@ -1,10 +1,13 @@
 'use strict';
 
 {
-  const timer = document.getElementById('timer');
-  const start = document.getElementById('start');
-  const stop  = document.getElementById('stop');
-  const reset = document.getElementById('reset');
+  const timer = document.getElementById("timer");
+  const start = document.getElementById("start");
+  const stop = document.getElementById("stop");
+  const reset = document.getElementById("reset");
+
+  // ボタンの初期状態を定義
+  setButtonStateInitial();
 
   // 最初にスタートボタンを押した時の時間
   let startTime;
@@ -12,7 +15,8 @@
   let elapsedTime = 0;
 
   // スタートボタン押下時のイベント
-  start.addEventListener('click', () => {
+  start.addEventListener("click", () => {
+    setButtonStateRunnning();
     // スタートボタン押下時の時刻を取得
     startTime = Date.now();
     // 時刻のカウントアップ関数を呼び出し
@@ -24,9 +28,9 @@
     // この関数が呼び出された時の時間からスタートボタンをクリックした時の時間を引く。さらに経過時間があれば足す。
     const time = new Date(Date.now() - startTime + elapsedTime);
     // 分、秒、ミリ秒を取得し、２桁（３桁）かつ満たない場合は０で埋める
-    const minutes = String(time.getMinutes()).padStart(2, '0');
-    const seconds = String(time.getSeconds()).padStart(2, '0');
-    const milliSeconds = String(time.getMilliseconds()).padStart(3, '0');
+    const minutes = String(time.getMinutes()).padStart(2, "0");
+    const seconds = String(time.getSeconds()).padStart(2, "0");
+    const milliSeconds = String(time.getMilliseconds()).padStart(3, "0");
     timer.textContent = `${minutes}:${seconds}:${milliSeconds}`;
 
     // 10ミリ秒経過ごとに自身を呼び出すことで、スタートボタン押下時からの経過時間が10ミリ秒ごとに表示される
@@ -36,7 +40,8 @@
   }
 
   // ストップボタン押下時のイベント
-  stop.addEventListener('click', () => {
+  stop.addEventListener("click", () => {
+    setButtonStateStopped();
     // setTimeout()の解除
     clearTimeout(timeOutId);
     // ストップ押下時の時間からスタートボタン押下時の時間を引いて経過時間を取得
@@ -44,9 +49,30 @@
   });
 
   // リセットボタン押下時のイベント
-  reset.addEventListener('click', () => {
-    timer.textContent = '00:00.000';
+  reset.addEventListener("click", () => {
+    setButtonStateInitial();
+    timer.textContent = "00:00.000";
     elapsedTime = 0;
-  })
+  });
 
+  // 初期画面でのボタンの非活性化設定
+  function setButtonStateInitial() {
+    stop.disabled = true;
+    reset.disabled = true;
+    start.disabled = false;
+  }
+
+  // タイマーが走っている時のボタンの非活性化設定
+  function setButtonStateRunnning() {
+    stop.disabled = false;
+    reset.disabled = true;
+    start.disabled = true;
+  }
+
+  // タイマーがストップしている時のボタンの非活性化設定
+  function setButtonStateStopped() {
+    stop.disabled = true;
+    reset.disabled = false;
+    start.disabled = false;
+  }
 }
