886cbc32e966fa239b3444e333a273c23c6fcee0
takaya-47 <local.s.s.47@gmail.com>
2022-09-02 08:47:41 +0900

    数字タッチゲームのJS完成

--- js/touch.js
+++ js/touch.js
@@ -35,7 +35,7 @@
         this.ele.classList.add("pressed");
         this.game.addCurrentNum();
         // 全てのパネルを押し終わったらタイマーをストップする
-        if (this.game.getCurrentNum() === 4) {
+        if (this.game.getCurrentNum() === this.game.getLevel() ** 2) {
           clearTimeout(this.game.getTimeoutId());
         }
       }
@@ -46,7 +46,7 @@
     constructor(game) {
       this.game = game; // Gameクラスのインスタンス
       this.panels = [];
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
         this.panels.push(new Panel(this.game));
       }
       this.setUp();
@@ -67,9 +67,12 @@
      * ※処理自体はパネルクラスのメソッドで行っています。
      */
     activate() {
-      const nums = [0, 1, 2, 3];
+      const nums = [];
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+        nums.push(i);
+      }
       this.panels.forEach((panel) => {
-        // numsから0〜3までのランダムな位置から１つ取り出してnumに格納
+        // numsからランダムな位置から１つ取り出してnumに格納
         const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
         panel.activate(num);
       })
@@ -77,7 +80,8 @@
   }
 
   class Game {
-    constructor() {
+    constructor(level) {
+      this.level = level;
       this.board = new Board(this); // thisはGameクラスのインスタンス
       this.currentNum = undefined;
       this.startTime = undefined;
@@ -87,13 +91,7 @@
       button.addEventListener('click', () => {
         this.start();
       });
-    }
-
-    /**
-     * currentNumに１を加算します。
-     */
-    addCurrentNum() {
-      this.currentNum++;
+      this.setUp();
     }
 
     /**
@@ -108,6 +106,31 @@
      */
     getTimeoutId() {
       return this.timeoutId;
+    }
+
+    /**
+     * levelのゲッター
+     */
+    getLevel() {
+      return this.level;
+    }
+
+    /**
+     * currentNumに１を加算します。
+     */
+    addCurrentNum() {
+      this.currentNum++;
+    }
+
+    /**
+     * レベルに応じてCSSを動的に定義します。
+     */
+    setUp() {
+      const container = document.getElementById('container');
+      const panelWidth = 50;
+      const boardPadding = 10;
+        /* 50px * 2 + 10px * 2 */
+      container.style.width = panelWidth * this.level + boardPadding * 2 + 'px';
     }
 
     /**
@@ -138,5 +161,6 @@
     }
   }
 
-  new Game();
+  // レベルを設定してGameインスタンスを生成
+  new Game(9);
 }
